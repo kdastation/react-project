8deaf83972b10d96a9b9ebf1fca288d4daf03d9e
Denis <mdo@liis.su>
2023-02-04 18:42:59 +0300

    added async reducers in store decorator

--- src/app/providers/StoreProvider/config/store..ts
+++ src/app/providers/StoreProvider/config/store..ts
@@ -1,12 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { createReducerManager } from 'app/providers/StoreProvider/lib/createReducerManager';
 import { State } from './storeTypes';
 import { rootReducer } from './rootReducer';
 
 export const createReduxStore = (
   initialState?: State,
+  asyncReducers?: ReducersMapObject<State>,
 ) => {
-  const reducerManager = createReducerManager(rootReducer);
+  const reducerManager = createReducerManager({
+    ...asyncReducers,
+    ...rootReducer,
+  });
 
   const store = configureStore<State>({
     reducer: reducerManager.reduce,
