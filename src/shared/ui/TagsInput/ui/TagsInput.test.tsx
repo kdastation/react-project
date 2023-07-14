306f5d85de0eba54359ba561e3ec27efc28e9c87
Denis <mdo@liis.su>
2023-07-14 11:00:03 +0300

    added remove tag and added tag when typing comma

--- src/shared/ui/TagsInput/ui/TagsInput.test.tsx
+++ src/shared/ui/TagsInput/ui/TagsInput.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { getByTestId, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { testsIds } from "../consts/testsIds";
 import { TagsInput } from "./TagsInput";
@@ -15,13 +15,44 @@ describe("TagsInput", () => {
     tags.forEach((tag, index) => {
       const elementTag = tagsElements[index];
 
-      expect(elementTag.textContent).toBe(tag);
+      expect(elementTag.textContent?.includes(tag)).toBe(true);
     });
   });
 
-  it.todo("should be able to remove tag");
+  it("should be able to remove tag", async () => {
+    const onChange = jest.fn();
+    const tags = ["a", "b", "c"];
 
-  it.todo("should add tag when typing comma");
+    render(<TagsInput tags={tags} onChange={onChange} />);
+
+    const firstTag = screen.getAllByTestId(testsIds.tag)[0];
+
+    const removeTrigger = getByTestId(firstTag, testsIds.removeTrigger);
+
+    await userEvent.click(removeTrigger);
+
+    expect(onChange).toHaveBeenCalledWith(["b", "c"]);
+  });
+
+  it("should add tag when typing comma", async () => {
+    const onChange = jest.fn();
+
+    const { rerender } = render(<TagsInput tags={[]} onChange={onChange} />);
+
+    const input = screen.getByTestId(testsIds.input) as HTMLInputElement;
+
+    await userEvent.type(input, "tag-one,");
+
+    expect(onChange).toHaveBeenCalledWith(["tag-one"]);
+
+    expect(input.value).toBe("");
+
+    rerender(<TagsInput tags={["tag-one"]} onChange={onChange} />);
+
+    await userEvent.type(input, "tag-two,");
+
+    expect(onChange).toHaveBeenCalledWith(["tag-one", "tag-two"]);
+  });
 
   it("should be able to add tag when pressing enter", async () => {
     const onChange = jest.fn();
--- src/shared/ui/TagsInput/ui/TagsInput.tsx
+++ src/shared/ui/TagsInput/ui/TagsInput.tsx
@@ -9,8 +9,20 @@ type TagsInputProps = {
 export const TagsInput: FC<TagsInputProps> = ({ tags = [], onChange }) => {
   const [value, setValue] = useState("");
 
+  const addTag = (newTag: string) => {
+    onChange?.([...tags, newTag]);
+  };
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
+
+    if (value.includes(",")) {
+      const lastValue = value.split(",")[0];
+
+      addTag(lastValue);
+      setValue("");
+      return;
+    }
 
     setValue(value);
   };
@@ -19,15 +31,28 @@ export const TagsInput: FC<TagsInputProps> = ({ tags = [], onChange }) => {
     const isEnter = event.key === "Enter";
 
     if (isEnter) {
-      onChange?.([...tags, value]);
+      addTag(value);
       setValue("");
     }
+  };
+
+  const handleRemoveTag = (removeTag: string) => {
+    const updatedTags = tags.filter((tag) => tag !== removeTag);
+    onChange?.(updatedTags);
   };
 
   return (
     <div>
       {tags.map((tag) => (
-        <div data-testid={testsIds.tag}>{tag}</div>
+        <div data-testid={testsIds.tag}>
+          {tag}
+          <div
+            data-testid={testsIds.removeTrigger}
+            onClick={() => handleRemoveTag(tag)}
+          >
+            remove
+          </div>
+        </div>
       ))}
       <input
         data-testid={testsIds.input}
