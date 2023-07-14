1d496cd430f9242ef44b42d6d5ecb0cbcbd77b7e
Denis <mdo@liis.su>
2023-07-14 16:56:18 +0300

    added use debounce

--- src/features/FiltersArticles/ui/Search/Search.tsx
+++ src/features/FiltersArticles/ui/Search/Search.tsx
@@ -4,17 +4,21 @@ import { rootSelector } from "../../selectors/rootSelector";
 import { actions } from "../../model/slice";
 import { fetchArticles } from "@/entities/Article/Articles";
 import { useAppDispatch } from "@/shared/lib/hooks/useAppDispatch";
+import { useDebounce } from "@/shared/lib/hooks/useDebounce";
 
 export const Search = () => {
   const dispatch = useAppDispatch();
   const search = useSelector(rootSelector.selectSearch);
+  const debounceFetchFilters = useDebounce((args) => {
+    dispatch(fetchArticles(args));
+  }, 100);
 
   const handleChangeSearch = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
 
     dispatch(actions.setSearch(value));
 
-    dispatch(fetchArticles({ search: value }));
+    debounceFetchFilters({ search: value });
   };
 
   return <input type="text" value={search} onChange={handleChangeSearch} />;
