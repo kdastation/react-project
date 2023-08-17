030aef39ac4a250d8afb87576abb79bb62c1f65d
kdastation <mrkn.denis.20@gmail.com>
2023-08-17 21:03:10 +0300

    update invalidate tags

--- src/features/Playlist/EditPlaylist/model/api/api.ts
+++ src/features/Playlist/EditPlaylist/model/api/api.ts
@@ -1,18 +1,19 @@
 import { rtkApi, tags } from "@/shared/api";
 import { Id } from "@/shared/types/Id";
+import { Playlist } from "@/entities/Playlist";
 
 type Args = {
   name: string;
 };
 export const api = rtkApi.injectEndpoints({
   endpoints: (build) => ({
-    editPlaylist: build.mutation<void, DeepPartial<Args> & { id: Id }>({
+    editPlaylist: build.mutation<Playlist, DeepPartial<Args> & { id: Id }>({
       query: ({ id, ...body }) => ({
         url: `/playlist/${id}`,
         method: "PUT",
         body,
       }),
-      invalidatesTags: [{ type: tags.PLAYLIST_TAG }],
+      invalidatesTags: (res) => [{ type: tags.PLAYLIST_TAG, id: res?.id }],
     }),
   }),
 });
