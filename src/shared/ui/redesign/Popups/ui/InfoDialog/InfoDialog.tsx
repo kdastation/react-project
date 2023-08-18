eb1e0eb02c645d9fb5a19bcf95c5856c7df417dd
kdastation <mrkn.denis.20@gmail.com>
2023-08-18 12:52:06 +0300

    added styles info dialog

--- src/shared/ui/redesign/Popups/ui/InfoDialog/InfoDialog.tsx
+++ src/shared/ui/redesign/Popups/ui/InfoDialog/InfoDialog.tsx
@@ -1,17 +1,19 @@
 import { ReactNode } from "react";
 import { Modal, ModalProps } from "../Modal/Modal";
+import { HStack, VStack } from "@/shared/ui/Stack";
+import styles from "./InfoDiagol.module.scss";
 
-type Props = ModalProps & {
+type Props = Omit<ModalProps, "closeIcon"> & {
   text: ReactNode;
 };
 
 export const InfoDialog = ({ text, ...modalProps }: Props) => (
-  <Modal {...modalProps}>
-    <div>
-      {text}
-      <div>
+  <Modal {...modalProps} closeIcon={false}>
+    <VStack className={styles.container} gap="16">
+      <div>{text}</div>
+      <HStack max justify="end">
         <button onClick={modalProps.onClose}>OK</button>
-      </div>
-    </div>
+      </HStack>
+    </VStack>
   </Modal>
 );
--- src/shared/ui/redesign/Popups/ui/Modal/Modal.tsx
+++ src/shared/ui/redesign/Popups/ui/Modal/Modal.tsx
@@ -1,14 +1,22 @@
 import { ReactNode } from "react";
 import { Overlay, OverlayProps } from "@/shared/ui/redesign/Popups/ui/Overlay/Overlay";
 import styles from "./Modal.module.scss";
+import Close from "@/shared/assets/icons/close.svg";
 
 export type ModalProps = Pick<OverlayProps, "isOpen" | "onClose" | "children"> & {
   title?: ReactNode;
+  closeIcon?: boolean;
 };
 
-export const Modal = ({ children, title, ...props }: ModalProps) => (
-  <Overlay {...props}>
+// TODO: Add ButtonComponent
+export const Modal = ({ children, title, onClose, closeIcon = true, ...props }: ModalProps) => (
+  <Overlay {...props} onClose={onClose}>
     <div className={styles.container}>
+      {closeIcon && (
+        <button onClick={onClose} className={styles.close}>
+          <Close width={24} height={24} fill="currentColor" />
+        </button>
+      )}
       {title && <div className={styles.title}>{title}</div>}
       {children}
     </div>
