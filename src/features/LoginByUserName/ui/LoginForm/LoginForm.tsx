638070e4354a83c436e93b9bd518fd1a3ca4cd5a
kdastation <mrkn.denis.20@gmail.com>
2024-04-03 22:53:16 +0300

    fixed

--- src/features/LoginByUserName/ui/LoginForm/LoginForm.tsx
+++ src/features/LoginByUserName/ui/LoginForm/LoginForm.tsx
@@ -65,7 +65,7 @@ export const LoginForm: FC<LoginFormProps> = ({ onSuccess }) => {
           value={password}
         />
         <Button data-testid="SumbitLoginButton" onClick={handleLogin}>
-          asdasdasdsadasdasdasda
+          Войти
         </Button>
       </div>
     </DynamicModuleLoader>
